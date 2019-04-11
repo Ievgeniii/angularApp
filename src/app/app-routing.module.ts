7fe5611755caed19ea41d79bb512727877cf0e2f
Ievgenii <evgmilk21@gmail.com>
2019-04-11 15:34:54 +0300

    routerImproved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomepageComponent } from './core/homepage/homepage.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 let routes: Routes = [
-  {path: '', component: HomepageComponent}
-]
+  {path: '', component: HomepageComponent},
+  {path: 'not-found', component: PageNotFoundComponent},
+  {path: '**', redirectTo: '/not-found'}
+];
 
 @NgModule({
   imports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,20 @@ import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { GalleryModule } from './gallery/gallery.module';
 import { UserModule } from './users/user.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     GalleryModule,
+    UserModule,
     AppRoutingModule,
     CoreModule,
-    HttpClientModule,
-    UserModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
